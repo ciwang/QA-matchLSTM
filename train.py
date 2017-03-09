a29a24b4c1d9a60590bb63e2734f2fdc7f45fd6f
Annie Hu <anniehu@stanford.edu>
2017-03-09 14:13:05 -0800

    initial push with data loading fxn in train.py

--- train.py
+++ train.py
@@ -7,10 +7,12 @@ import json
 
 import tensorflow as tf
 
-from qa_model import Encoder, QASystem, Decoder
+# from qa_model import Encoder, QASystem, Decoder
 from os.path import join as pjoin
 
 import logging
+
+from collections import defaultdict
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,8 +23,8 @@ tf.app.flags.DEFINE_float("dropout", 0.15, "Fraction of units randomly dropped o
 tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use during training.")
 tf.app.flags.DEFINE_integer("epochs", 10, "Number of epochs to train.")
 tf.app.flags.DEFINE_integer("state_size", 200, "Size of each model layer.")
-tf.app.flags.DEFINE_integer("output_size", 750, "The output size of your model.") #size of largest paragraph context size, need to change for baseline model
-tf.app.flags.DEFINE_integer("embedding_size", 50, "Size of the pretrained vocabulary.")
+tf.app.flags.DEFINE_integer("output_size", 400, "The output size of your model.")
+tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained vocabulary.")
 tf.app.flags.DEFINE_string("data_dir", "data/squad", "SQuAD directory (default ./data/squad)")
 tf.app.flags.DEFINE_string("train_dir", "train", "Training directory to save the model parameters (default: ./train).")
 tf.app.flags.DEFINE_string("load_train_dir", "", "Training directory to load model parameters from to resume training (default: {train_dir}).")
@@ -31,7 +33,7 @@ tf.app.flags.DEFINE_string("optimizer", "adam", "adam / sgd")
 tf.app.flags.DEFINE_integer("print_every", 1, "How many iterations to do per print.")
 tf.app.flags.DEFINE_integer("keep", 0, "How many checkpoints to keep, 0 indicates keep all.")
 tf.app.flags.DEFINE_string("vocab_path", "data/squad/vocab.dat", "Path to vocab file (default: ./data/squad/vocab.dat)")
-tf.app.flags.DEFINE_string("embed_path", "./data/squad/glove.trimmed.50.npz", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
+tf.app.flags.DEFINE_string("embed_path", "", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
 
 FLAGS = tf.app.flags.FLAGS #goes everywhere else in model
 
@@ -54,7 +56,9 @@ def initialize_vocab(vocab_path):
         rev_vocab = []
         with tf.gfile.GFile(vocab_path, mode="rb") as f:
             rev_vocab.extend(f.readlines())
+        # index to word
         rev_vocab = [line.strip('\n') for line in rev_vocab]
+        # word to index
         vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
         return vocab, rev_vocab
     else:
@@ -76,26 +80,63 @@ def get_normalized_train_dir(train_dir):
     os.symlink(os.path.abspath(train_dir), global_train_dir)
     return global_train_dir
 
+def load_dataset(f1, f2, f3, batch_size):
+    fd1, fd2, fd3 = open(f1), open(f2), open(f3)
+    batch = []
+    while True:
+        line1, line2, line3 = (fd1.readline().rstrip(),
+                                fd2.readline().rstrip(),
+                                fd3.readline().rstrip())
+        if not line1:
+            break
+        batch.append((line1, line2, line3))
+
+        if len(batch) == batch_size:
+            yield batch
+
+def generate_histograms(dataset):
+    question_lengths = defaultdict(int)
+    context_lengths = defaultdict(int)
+    answer_lengths = defaultdict(int)
+
+    for batch in dataset:
+        for line in batch:
+            question = len(line[0].split()) // 10
+            context = len(line[1].split()) // 10
+            span = (int(line[2].split()[1]) - int(line[2].split()[0])) // 10
+            question_lengths[question] += 1
+            context_lengths[context] += 1
+            answer_lengths[span] += 1
+
+    print(question_lengths)
+    print(context_lengths)
+    print(answer_lengths)
 
 def main(_):
 
     # Do what you need to load datasets from FLAGS.data_dir
-    dataset = [(1,1,1), (1,1,1)]
-    # pass in path, read in data using function that is not provided
-    # use python generator to read batch at a time
-    # python has a readline type API
+    # use .readlines() to load file ourselves
+    # use python generator
+    question_path = pjoin(FLAGS.data_dir, "train.ids.question")
+    context_path = pjoin(FLAGS.data_dir, "train.ids.context")
+    answer_path = pjoin(FLAGS.data_dir, "train.span")
 
-    #loads embedding
+    # for testing
+    dataset = [(1,1,1), (1,1,1)]
+    # dataset = load_dataset(question_path, context_path, answer_path, 1000)
+    # generate_histograms(dataset)
+
+    # loads embedding
     embed_path = FLAGS.embed_path or pjoin("data", "squad", "glove.trimmed.{}.npz".format(FLAGS.embedding_size))
     vocab_path = FLAGS.vocab_path or pjoin(FLAGS.data_dir, "vocab.dat")
-    vocab, rev_vocab = initialize_vocab(vocab_path) #one is list and one is dict
+    vocab, rev_vocab = initialize_vocab(vocab_path) # one is list and one is dict
 
     encoder = Encoder(size=FLAGS.state_size, vocab_dim=FLAGS.embedding_size)
     decoder = Decoder(output_size=FLAGS.output_size)
 
     qa = QASystem(encoder, decoder, FLAGS)
 
-    #log file
+    # log file
     if not os.path.exists(FLAGS.log_dir):
         os.makedirs(FLAGS.log_dir)
     file_handler = logging.FileHandler(pjoin(FLAGS.log_dir, "log.txt"))
@@ -105,7 +146,7 @@ def main(_):
     with open(os.path.join(FLAGS.log_dir, "flags.json"), 'w') as fout:
         json.dump(FLAGS.__flags, fout)
 
-    #start training
+    # start training
     with tf.Session() as sess:
         load_train_dir = get_normalized_train_dir(FLAGS.load_train_dir or FLAGS.train_dir)
         initialize_model(sess, qa, load_train_dir)
